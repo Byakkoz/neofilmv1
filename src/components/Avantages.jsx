4f143dc69b417a3519bd0ce969a888cd0731e516
Lucas <bellurlucas@gmail.com>
2024-11-25 22:15:02 +0100

    nouvellesmodifencore6

--- src/components/Avantages.jsx
+++ src/components/Avantages.jsx
@@ -181,10 +181,13 @@ const Avantages = () => {
   }`}
   style={{
     marginTop: isWidth1440 ? '-200px' : '12px', // Remonte l'élément pour 1440px
+    width: isWidth1440 ? '90%' : '100%', // Réduit la taille pour 1440px
+    height: isWidth1440 ? 'auto' : 'auto', // Ajustement si nécessaire
   }}
 >
   <ImageSlider />
 </div>
+
 
     </section>
   );
